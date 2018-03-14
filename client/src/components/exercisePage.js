c7536241074d4aeff2712c6d61c260db7497dae2
Christina Moore <sayhitochristina@gmail.com>
2018-03-14 01:07:54 -0500

    work with svg

--- client/src/components/exercisePage.js
+++ client/src/components/exercisePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ExerciseForm from './addExerciseForm'
 import BrowseExercises from './browseExercises';
+import MuscleMap from './muscleMap';
+
 
 export default class SignupPage extends React.Component {
   render() {
     return (
       <div>
       <ExerciseForm />
+      <MuscleMap />
       <BrowseExercises />
       </div>
     )
