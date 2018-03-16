ff595700ed803bf233b7992af50c8e450f89f3eb
Christina Moore <sayhitochristina@gmail.com>
2018-03-16 12:53:41 -0500

    SVG WORKING

---
+++ client/src/components/generateColorMap.js
@@ -0,0 +1,39 @@
+const dbValues = {
+  "Arms": 61,
+  "Glutes": 1,
+  "Legs": 1
+}
+
+const keys = {
+  0: 'black',
+  1: 'yellow',
+  2: 'orange',
+  3: 'red' // greater than 3
+}
+
+
+
+
+export const generateColorMap = (dbValues) => {
+  const keys = {
+    0: 'black',
+    1: 'yellow',
+    2: 'orange',
+    3: 'red' // greater than 3
+  }
+  let returnObj = {};
+  for (let muscleGroup in dbValues) {
+    let muscleE = dbValues[muscleGroup];
+    if (muscleE > 3 ) {
+      muscleE = 3;
+    }
+    if (muscleE in keys) {
+      returnObj[muscleGroup] = keys[muscleE]
+    }
+  }
+  return returnObj
+}
+
+// generateColorMap(dbValues, keys)
+// { Arms: 'red', Glutes: 'yellow', Legs: 'yellow' }
+
--- client/src/components/muscles/workedMuscles.js
+++ client/src/components/muscles/workedMuscles.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getMusclesFromWorkout} from '../../actions/workout'
 import MuscleMap from '../muscleMap';
+import {generateColorMap} from '../generateColorMap';
+// console.log(generateColorMap)
 
 
 class WorkedMuscles extends React.Component {
@@ -13,25 +15,41 @@ class WorkedMuscles extends React.Component {
   // TODO re-render when log a different workout!!!!!!!!!!!!!!
 
   render () {
-    console.log('Worked muscles mount')
     let musclesUsed;
-    let recentW = this.props.recentWorkout
+    let colorMap;
+    const recentW = this.props.recentWorkout
+    const usedMuscles = this.props.recentMuscles;
   
-    if (this.props.loggedIn && this.props.recentWorkout) {
-      musclesUsed = this.props.recentMuscles.map((muscle, index) => 
-        <div key={index}>
-        <h1>{muscle}</h1>
-        </div>)
-    } else return <h1>Please log a workout</h1>
+    // if (this.props.loggedIn && this.props.recentWorkout) {
+    //   musclesUsed = this.props.recentMuscles.map((muscle, index) => 
+    //     <div key={index}>
+    //     <h1>{muscle}</h1>
+    //     </div>)
+    // } else return <h1>Please log a workout</h1>
   
     // color = {3: 'red'}
     // muscles = {'arms': 3}
+    // const keys = {
+    //   0: 'black',
+    //   1: 'yellow',
+    //   2: 'orange',
+    //   3: 'red' // greater than 3
+    // }
+    console.log(usedMuscles)
+
+    if (usedMuscles !== []) {
+      colorMap = generateColorMap(usedMuscles)
+      console.log(colorMap);
+      console.log(colorMap.Arms)
+
+    }
+
     return (<div>
       <h1>{`Your most recent workout was ${recentW}`}</h1>
       {musclesUsed} {/* SVG */}
       <MuscleMap 
-        chestColor='yellow'
-        armColor='green'
+        chestColor={colorMap.Chest || 'black'}
+        armColor={colorMap.Arms}
       />
       </div>)
   }
